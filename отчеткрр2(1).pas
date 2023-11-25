fdc5fe7c5c8fef2201f8557c86cd286fff383c88
whiteraven326 <144677455+whiteraven326@users.noreply.github.com>
2023-11-25 20:51:42 +0300

    Add files via upload

---
+++ отчеткрр2(1).pas
@@ -0,0 +1,21 @@
+﻿program funfrog;
+var
+arr: array [1..10] of integer; // массив
+n, s, k, sum: integer; // длина массива, заданное число, минимальное k, сумма первых k элементов
+begin
+writeln('Введите длину массива: ');
+readln(n);
+writeln('Введите элементы массива: ');
+for k := 1 to n do
+read(arr[k]);
+writeln('Введите заданное число: ');
+readln(s);
+k := 1; // начальное значение для k
+sum := arr[k]; // начальное значение для суммы
+while (sum <= s) and (k < n) do
+begin
+k := k + 1;
+sum := sum + arr[k];
+end;
+writeln('Порядковый номер минимального k: ', k);
+end.
---
+++ отчеткрр2(2).pas
@@ -0,0 +1,40 @@
+﻿program fun;
+var s1, s2, t: string;
+  i, a, b, k: integer;
+begin
+  write('Введите строку: ');
+  readln(s1);
+  write('Введите подстроку: ');
+  readln(s2);
+  // нахождение первого и последнего вхождения //
+  i:= 1;
+  while (i<=length(s1)) and (length(s2)>0) do
+  begin
+    k:= 0;
+    while ((i + k)<=length(s1)) and (k<length(s2)) and (s1[i+k] = s2[k+1]) do inc(k);
+    if k=Length(s2) then
+    begin
+      if a=0 then a:= i
+      else b:= i;
+      inc(i, k);
+    end
+    else inc(i);
+  end;
+  // удаление подстрок из строки //
+  t:= '';
+  if a>0 then
+  begin
+    i:= 1;
+    while i<=Length(s1) do
+    begin
+      if (i=a) or (i=b) then inc(i, Length(s2))
+      else
+      begin
+        t:= t+s1[i];
+        inc(i);
+      end;
+    end;  
+    s1:= t;
+  end;
+  writeln('Результат:',s1);
+end.
